ae118cb89137f9ad47f659d24d68f979313d960f
KA101 <cstrayer@rocketmail.com>
2014-02-13 19:08:01 -0500

    Merge pull request #6086 from Rivet-the-Zombie/bio_face_mask-fix
    Makes bio_face_mask a passive bionic.
    Conflicts:
    	data/json/bionics.json
    	data/json/items/bionics.json
    	src/npctalk.cpp

--- src/npctalk.cpp
+++ src/npctalk.cpp
@@ -441,9 +441,9 @@ void npc::talk_to_u()
  if (d.topic_stack.back() == TALK_NONE) {
   d.topic_stack.back() = pick_talk_topic(&(g->u));
  }
- 
+
  moves -= 100;
- 
+
  if(g->u.has_disease("deaf")) {
   g->add_msg(_("%s tries to talk to you, but you're deaf!"), name.c_str());
   if(d.topic_stack.back() == TALK_MUG) {
@@ -452,7 +452,7 @@ void npc::talk_to_u()
   }
   return;
  }
- 
+
  decide_needs();
 
  d.win = newwin(FULL_SCREEN_HEIGHT, FULL_SCREEN_WIDTH,
@@ -1549,6 +1549,10 @@ int trial_chance(talk_response response, player *u, npc *p)
     chance += 40;
    if (u->has_trait("ELFAEYES"))
     chance += 10;
+   if (u->has_bionic("bio_voice")) //come on, who would suspect a robot of lying?
+    chance += 10;
+   if (u->has_bionic("bio_face_mask"))
+    chance += 20;
    break;
 
   case TALK_TRIAL_PERSUADE:
@@ -1557,6 +1561,8 @@ int trial_chance(talk_response response, player *u, npc *p)
    if (u->has_trait("ELFAEYES"))
     chance += 20;
    if (u->has_trait("TAIL_FLUFFY"))
+    chance += 10;
+   if (u->has_bionic("bio_face_mask"))
     chance += 10;
    if (u->has_trait("GROWL"))
     chance -= 25;
@@ -1589,6 +1595,14 @@ int trial_chance(talk_response response, player *u, npc *p)
     chance += 15;
    if (u->has_trait("SNARL"))
     chance += 30;
+   if (u->has_bionic("bio_face_mask"))
+    chance += 10;
+   if (u->has_bionic("bio_armor_eyes"))
+    chance += 10;
+   if (u->has_bionic("bio_deformity"))
+    chance += 20;
+   if (u->has_bionic("bio_voice"))
+    chance += 20;
    break;
 
  }
