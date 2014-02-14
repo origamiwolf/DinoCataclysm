44aae8a5665b5c4ba6f8e5370aab07908645259a
KA101 <cstrayer@rocketmail.com>
2014-02-13 23:09:49 -0500

    Merge branch 'shoggoth' of https://github.com/Soyweiser/Cataclysm-DDA into Soyweiser-shoggoth
    Conflicts:
    	src/monattack.cpp

--- src/mongroupdef.cpp
+++ src/mongroupdef.cpp
@@ -428,6 +428,7 @@ void init_translation() {
     monStr2monId["mon_breather_hub"] = mon_breather_hub;
     monStr2monId["mon_breather"] = mon_breather;
     monStr2monId["mon_shadow_snake"] = mon_shadow_snake;
+    monStr2monId["mon_shoggoth"] = mon_shoggoth;
     monStr2monId["mon_eyebot"] = mon_eyebot;
     monStr2monId["mon_manhack"] = mon_manhack;
     monStr2monId["mon_skitterbot"] = mon_skitterbot;
